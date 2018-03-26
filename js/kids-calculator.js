254c497430db66ee31466cbcc901c116f501d5b7
ralu <ralucaignat56@gmail.com>
2018-03-26 20:58:06 +0300

    dcd

--- js/kids-calculator.js
+++ js/kids-calculator.js
@@ -1,23 +1,38 @@
 function countTo(n) {
 
-    for (var i = 1; i <= n;){
+    for (; i <= n; i += 5) {
         console.info(i);
         i++;
-
     }
 
 
 }
+
 function addNumbers(a, b) {
-     console.info ('primul nr este ' + a);
-     console.info ('al doilea  nr este ' + b);
-     for (;b > 0;) {
-        a++;  // il punem la a
-        b--; // luam un deget de la b
-         console.info(a +  ' au  mai ramas ' + b +  'degetele')
-     }
-    console.info('suma este: ' +a)
+    console.info('primul nr este ' + a);
+    console.info('al doilea  nr este ' + b);
+
+    if (a < b) {
+        var suma = a; // este nr cel mai mare
+        var degete = b; // este nr cel mai mic
+
+    }
+
+
+    while (degete > 0) {
+        degete--; //luam un deget de la b
+        suma++; // il punem la a
+        console.info(suma + ' - au mai ramas ' + degete + ' degetele');
+    }
+    console.info('suma este:' + suma);
+
+    // for (; degete > 0;) {
+    //     degete--; // // luam un deget de la b
+    //     suma++;  // il punem la a
+    //     console.info(suma + ' au  mai ramas ' + degete + 'degetele');
+    // }
+    // console.info('suma este: ' + suma);
 }
 
 
-var count = addNumbers(6, 3);
+var count = addNumbers(2, 6);
