cf5a088b747e8eb4a091da9e83624b5d93c45517
ralu <ralucaignat56@gmail.com>
2018-03-26 20:04:36 +0300

    4 in 2  8 in 10  am modificat titlul

---
+++ js/kids-calculator.js
@@ -0,0 +1,22 @@
+function countTo(n) {
+
+    for (var i = 1; i <= n;){
+        console.info(i);
+        i++;
+
+    }
+
+
+}
+function addNumbers(a, b) {
+     console.info ('primul nr este ' + a);
+     console.info ('al doilea  nr este ' + b);
+     for (;b > 0;b--) {
+        a++;
+         console.info(a +  ' au  mai ramas ' + b +  'degetele')
+     }
+    console.info('suma este: ' +a)
+}
+
+
+var count = addNumbers(6, 3);
