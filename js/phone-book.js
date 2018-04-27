01ff38303a53042e59b7f2781236ceff8c98cb0f
ralu <ralucaignat56@gmail.com>
2018-04-27 13:16:15 +0300

    phone-book as array of persons

--- js/phone-book.js
+++ js/phone-book.js
@@ -1,17 +1,22 @@
 
-function(){
-
+function getRow(firstName,lastName,phone){
+    return "<tr><td>" + firstName +"</td><td>" + lastName +"</td><td>" + phone +"</td></tr>";
 }
 
+var persons = [
+     getRow("Nicolae","Matei",233),
+     getRow("Andrei","Ilie",0),
+     getRow("Raluca", "Ignat", 777)
+];
+
+var rows = '';
+
+persons.forEach(function (person) {
+    rows += person;
+});
+
+$('#phone-book tbody').html(rows);
 
 
-var row1 = "<tr><td>Nicolae</td><td>Matei</td><td>0</td></tr>";
-var row2 = "<tr><td>Andrei</td><td>Ilie</td><td>0</td></tr>";
-
-
-//document.getElementById('phone-book').getElementsByTagName('tbody')[0].innerHTML = row1 +row2;
-
-
-$('#phone-book tbody').html(row1 + row2);
 
 
